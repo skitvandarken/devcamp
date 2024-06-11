f459a4b8bd28fde00bae15f7c83827bb9614cec6
Pedro Jorge <skitvandarken2013@gmail.com>
2024-06-11 13:05:50 +0100

    regresso ao escritório e dificuldades com a responsividade das cards

--- website/src/app/paginas/inicio/inicio.component.ts
+++ website/src/app/paginas/inicio/inicio.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../layout/navbar/navbar.component';
+import { RouterLink } from '@angular/router';
+import { RodapeComponent } from '../../layout/rodape/rodape.component';
+import { ParceirosComponent } from '../../layout/parceiros/parceiros.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    RouterLink,
+    RodapeComponent,
+    ParceirosComponent
+    
   ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
